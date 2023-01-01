c98d9aa71b013bc9563b36aa5234ee9c0cb0fe9b
Alexander Mendivil <alexanderima@outlook.es>
2022-12-31 18:04:24 -0700

    params url

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,80 @@
 <template>
     <h1>About page</h1>
+
+    <hr>
+    <!-- <h2>{{ name }}</h2> -->
+
+    <button @click="onChangeName">Cambiar nombre</button>
 </template>
+
+<script>
+
+    export default{
+        data(){
+            return{
+                name: 'Alex'
+            }
+        },
+
+        methods:{
+            onChangeName(){
+                console.log("Se cambiara el nombre")
+                this.name = 'Melisa'
+            }
+        },  
+        beforeCreate(){
+            console.log("beforeCreated")
+        },
+        created(){
+            console.log("Created")
+            
+        },
+        beforeMount(){
+            console.log("bedoreMount")
+            
+        },
+        mounted(){
+            console.log("Mount")
+            
+        },
+        beforeUpdate(){
+            console.log("beforeUpdate")
+            
+        },
+        updated(){
+            console.log("Update")
+            
+        },
+        activated(){
+            console.log("activated")
+            
+        },
+        deactivated(){
+            console.log("deactivated")
+            
+        },
+        beforeUnmount(){
+            console.log("beforeUnmount")
+            
+        },
+        unmounted(){
+            console.log("unmounted")
+            
+        },
+        errorCaptured(){
+            console.log("errorCaptured")
+            
+        },
+        renderTracked(){
+            console.log("renderTracked")
+            
+        },
+        renderTriggered(){
+            console.log("renderTriggered")
+
+        }
+        
+    }
+
+
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,14 @@ import { createRouter, createWebHashHistory } from "vue-router"
 const routes = [
     { path: '/', component:  ()=>  import(/*webPackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage.vue') },
     { path: '/about', component:  ()=>  import(/*webPackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage.vue') },
-    { path: '/id', component:  ()=>  import(/*webPackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage.vue') },
+    { 
+        path: '/:id',
+        component:  ()=>  import(/*webPackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage.vue'),
+        props: ( route ) => {
+            const id = +route.params.id
+            return isNaN(id) ? {id: 0} : {id: +route.params.id}
+        }
+    },
     { path: '/:pathMatch(.*)*', component:  ()=>  import(/*webPackChunkName: "NoFoundPage"*/'../modules/shared/pages/NoPageFound.vue') },
 ]
 
