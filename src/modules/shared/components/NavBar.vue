389fae84be38bb0d12a47659f2acc2c57393134a
Alexander Mendivil <alexanderima@outlook.es>
2023-01-08 15:46:09 -0700

    custom navbar

--- src/modules/shared/components/NavBar.vue
+++ src/modules/shared/components/NavBar.vue
@@ -1,13 +1,33 @@
 <template>
 
     <div>
-        <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+        <!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
         <router-link :to="{ name: 'pokemon-id', params:{id: 85} }">Pokemon id</router-link>
-        <router-link :to="{ name: 'about' }">About</router-link>
+        <router-link :to="{ name: 'about' }">About</router-link> -->
+        <custom-link v-for="link in links" :key="link.to" :link="link"/>
     </div>
 </template>
 
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    
+    data(){
+        return{
+            links: [
+                {to: '/home', name: 'Pokemons'},
+                {to: '/pokemonid/50', name: 'por ID'},
+                {to: '/about', name: 'about'},
+                {to: 'https://google.com', name: 'google'},
+            ]
+        }
+    },
+    components:{
+        CustomLink: defineAsyncComponent(()=> import('./CustomLink.vue') )
 
+    }
+}
+</script>
 <style scoped>
     div {
         padding: 0 30px;
